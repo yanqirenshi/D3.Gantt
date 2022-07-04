51cc81f423fe32f337af27605af0d1aa1b46f324
yanqirenshi <yanqirenshi@gmail.com>
2022-07-04 09:44:35 +0900

    add tabs

--- dev/src/Hero.js
+++ dev/src/Hero.js
@@ -1,6 +1,7 @@
-function Hero() {
+export default function Hero() {
     return (
         <section className="hero" style={{background:'#4d5aaf'}}>
+
           <div className="hero-body">
             <div className="container">
               <h1 className="title" style={{color:'#fff'}}>
@@ -8,8 +9,22 @@ function Hero() {
               </h1>
             </div>
           </div>
+
+          <div className="hero-foot">
+            <nav className="tabs is-boxed is-centered">
+              <div className="container">
+                <ul>
+                  <li className="is-active">
+                    <a>Output</a>
+                  </li>
+                  <li>
+                    <a>Input</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+
         </section>
     );
 }
-
-export default Hero;
